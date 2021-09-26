97822d0ec8be48dc3c6c18b02535c411c221ec1e
saurabh patel <83927859+Saurabh5612@users.noreply.github.com>
2021-09-25 21:23:13 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work-Day-Scheduler
+Timeblocks go here.
+disply current day &time.
+assign saveBtn click listener for user input and time stamp?
+get nearby values.
+taken the change from the sibling html description attribute.
+set items in local storage.
+load any saved data from localStorage.
+get current number of hours.
+loop over time blocks.
+check if we've moved past this time.
+
+!![day planner demo](.file:///C:/Users/Jaymin/Desktop/Work-Day-Scheduler/indx.html)
---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    // disply current day &time
+    $("#currentDay").text(moment() .format("MM/DD/YYYY, hh:mm:ss a"));
+
+   //assign saveBtn click listener for user input and time stamp?
+   $(".saveBtn").on("click", function () {
+    //get nearby values.
+
+    console.log(this);
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var time = $(this).parent().attr("id"); 
+    //set items in local storage.
+    localStorage.setItem(time, text);
+
+   })
+
+        // load any saved data from localStorage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour(); 
+
+         // loop over time blocks
+         $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+        
+            // check if we've moved past this time
+         if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else  {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })    
+ }        
+    hourTracker();
+
+})  
+  
+
+    
